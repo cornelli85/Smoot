62861b5eda6386bcaee3470d8cc657ea87d6ff74
Caesar <61262789+cornelli85@users.noreply.github.com>
2020-10-11 22:17:05 -0400

    first-edit

---
+++ script.js
@@ -0,0 +1,19 @@
+var questions = [
+    {
+        question1: "?",
+        choices: ["choice1", "choice2", "choice3", "choice4"],
+        correctChoice: "choice2"
+    }
+]
+
+console.log (questions[0])
+
+
+
+// var myCar = {
+//    make: 'Ford',
+//    model: 'Mustang',
+//    year: 1969
+// };
+
+
